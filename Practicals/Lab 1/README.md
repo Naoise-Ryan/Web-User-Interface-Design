17f53f42c45f239118e97eb29dee4d1ed564d680
(Student  C00281427) Naoise Ryan <C00281427@setu.ie>
2024-01-25 16:19:17 +0000

    Lab 1 progress

--- Practicals/Lab 1/README.md
+++ Practicals/Lab 1/README.md
@@ -1,7 +1,16 @@
 # Web-User-Interface-Design
 ## Lab 1
 ### Website Purpose
+Window sales website to advertise and sell windows
 
 ### Target Audience
+People who want to buy a window
 
 ### Key Features
+Displays information about the company and owner and has a selection of windows to choose from
+When you click the header the size and color of the first h1 on the page increases or decreases depending on if its already
+been clicked
+
+### References
+https://www.w3schools.com/jsref/met_document_queryselector.asp
+To understand querys and event listeners
--- Practicals/Lab 1/assets/js/script.js
+++ Practicals/Lab 1/assets/js/script.js
@@ -1,1 +1,21 @@
-alert("Welcome to the page")
+window.onload = function() {
+    alert("Welcome to the page")
+}
+
+let clicked = false 
+document.querySelector("Header").addEventListener("click", function() {
+    if (!clicked) {
+        changeStyle(true, "150px", "Yellow")
+    }
+    else {
+        changeStyle(false, "38px", "Black")
+    }
+});
+
+function changeStyle(clickedStatus, t_size, t_color){
+    clicked = clickedStatus;
+    //Stores the path to the first h1 on the page
+    let h1PathStyle = document.querySelector("h1").style;
+    h1PathStyle.fontSize = t_size;
+    h1PathStyle.color = t_color;
+}
